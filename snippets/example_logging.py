e51073258c0e5414cf8aad6c64160f547b2c3203
Mohit Mandokhot <mandokhotm@trcpg.com>
2022-12-20 11:37:38 -0500

    Logging to file and use of command line args

--- snippets/example_logging.py
+++ snippets/example_logging.py
@@ -1,14 +1,18 @@
 # Code Snippets.  
 # Logging and Type Hinting. 
+import sys
 import logging
 
-def add_ints(x: int,y: int) -> int:
+# Change Logging package's configurations
+logging.basicConfig(filename='example_logging.log',level=logging.DEBUG)
+
+def add_ints(x: int = 0,y: int = 0) -> int:
     """ Add two numbers. """
     return x+y
 
 if __name__ == "__main__":
-    x = 10
-    y = 25
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
     sum = add_ints(x,y)
     result = "When {} and {} are added, result is: {}".format(x,y,sum)
     logging.debug(result)
